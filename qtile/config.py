b2c71016de300daabc443d68ab1ca94d334441e8
Enubia <marcoschuster@mailbox.org>
2021-01-20 11:31:21 +0100

    opacity update

--- qtile/config.py
+++ qtile/config.py
@@ -394,8 +394,8 @@ widgets_screen2 = init_widgets_screen2()
 
 def init_screens():
     return [
-        Screen(top=bar.Bar(widgets=init_widgets_screen1(), size=26, opacity=0.8)),
-        Screen(top=bar.Bar(widgets=init_widgets_screen2(), size=26, opacity=0.8))
+        Screen(top=bar.Bar(widgets=init_widgets_screen1(), size=26, opacity=0.9)),
+        Screen(top=bar.Bar(widgets=init_widgets_screen2(), size=26, opacity=0.9))
     ]
 
 
