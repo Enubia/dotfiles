c52e28d94f670e682a5ab74e09454583e0942e3c
Enubia <marcoschuster@mailbox.org>
2021-01-20 14:11:27 +0100

    use nitrogen instead of variety

--- qtile/scripts/autostart.sh
+++ qtile/scripts/autostart.sh
@@ -33,7 +33,7 @@ function run {
 #run sxhkd -c ~/.config/qtile/sxhkd/sxhkdrc &
 
 #starting utility applications at boot time
-run variety &
+# run variety &
 run nm-applet &
 run pamac-tray &
 run xfce4-power-manager &
@@ -46,7 +46,7 @@ picom --config $HOME/.config/qtile/scripts/picom.conf &
 #starting user applications at boot time
 run volumeicon &
 #run discord &
-#nitrogen --restore &
+nitrogen --restore &
 #run caffeine -a &
 #run vivaldi-stable &
 #run firefox &
